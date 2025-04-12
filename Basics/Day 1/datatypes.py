c3aa1b0a945792af37548fb09a821832e86dfb4c
Musab <joiya4636@gmail.com>
2025-04-12 01:35:22 -0700

    Day 1 Baiscs

--- Basics/Day 1/datatypes.py
+++ Basics/Day 1/datatypes.py
@@ -2,3 +2,13 @@ name = "Musab"
 age=23 
 print ("My name is ", name)
 print ("My age is ", age)
+print (type(name))
+print (type(age))
+a=7
+b=2
+c=a//b# // is floor gives closest integer
+#  c= floor(c)
+print (c)
+
+price =input ("enter the price:")
+print("price is ", price)
---
+++ Basics/Day 1/sum.py
@@ -0,0 +1,5 @@
+a=2
+b=4
+sum= a+b
+print("The sum of a and b is:", sum)
+# python is implicit typed language
