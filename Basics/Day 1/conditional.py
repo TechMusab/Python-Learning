e75f31ffbfc174d7f69198ce4ff0cc3189b1e02e
Musab <joiya4636@gmail.com>
2025-04-12 01:40:10 -0700

    Day 1 Completed

--- Basics/Day 1/conditional.py
+++ Basics/Day 1/conditional.py
@@ -5,3 +5,27 @@ elif (age>=18):
     print ("you are eligible to vote")
 else:
     print ("you are not eligible to vote")
+#Relational
+c=4
+d=5
+print (c==d)
+print (c!=d)
+print (c<d)
+print (c>d)
+print (c<=d)
+print (c>=d)
+
+# Logical Operators
+a=True
+b=False
+print (a and b)
+print (a or b)
+print (not a)
+
+# Type Casting
+a=10
+b=20
+print (a+b)
+a=str(a)
+b=str(b)
+print (a+b)
