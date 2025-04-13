acff5c2a1719b6f6c0741f48230c8696c8242715
Musab <joiya4636@gmail.com>
2025-04-12 23:22:19 -0700

    Day 2

---
+++ Basics/Day2/strings.py
@@ -0,0 +1,11 @@
+str1= "this is a string"
+str2= ' this is also a string'
+str3= """ this is a string"""
+print(str1)
+str4= str1 + str2
+print(str4)
+print(len(str1))
+print(str1[0])
+# str[3]='J' not allowed
+print(str1[0:4])
+print(str1[0:])
