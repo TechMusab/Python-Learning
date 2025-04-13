3642a0638fb6a7b6fc1028783499a114b02da932
Musab <joiya4636@gmail.com>
2025-04-12 23:51:00 -0700

    largest of 4 numbers

--- Basics/Day2/practiceconditons.py
+++ Basics/Day2/practiceconditons.py
@@ -1,21 +1,30 @@
-number = int(input("Enter a number: "))
-if number % 2==0:
-    print("Even")
-else:
-    print("Odd")
+# number = int(input("Enter a number: "))
+# if number % 2==0:
+#     print("Even")
+# else:
+#     print("Odd")
 
 n1=int(input("Enter first number: "))
 n2=int(input("Enter second number: "))
 n3=int(input("Enter third number: "))
-if n1>n2 and n1>n3:
+n4=int(input("Enter fourth number:"))
+if n1>n2 and n1>n3 and n1>n4:
     print ("Largest number is: ",n1)
-elif n2>n3:
+elif n2>n3 and n2>n4:
     print ("Largest number is: ",n2)
-else:
+elif n3>n4:
     print ("Largest number is: ",n3)
-
-n4 = int(input("Enter a number: "))
-if n4 % 7==0:
-    print("Divisible by 7")
 else:
-    print("Not divisible by 7")
+    print ("Largest number is: ",n4)
+# if n1>n2 and n1>n3:
+#     print ("Largest number is: ",n1)
+# elif n2>n3:
+#     print ("Largest number is: ",n2)
+# else:
+#     print ("Largest number is: ",n3)
+
+# n4 = int(input("Enter a number: "))
+# if n4 % 7==0:
+#     print("Divisible by 7")
+# else:
+#     print("Not divisible by 7")
