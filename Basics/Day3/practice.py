7828b330eaaa954b79e967d948c876dccba32c64
Musab <joiya4636@gmail.com>
2025-04-14 00:35:36 -0700

    practice Day 3

---
+++ Basics/Day3/practice.py
@@ -0,0 +1,17 @@
+# m1=input("Enter 1st favourtie movie:")
+# m2=input("Enter 2nd favourtie movie:")
+# m3=input("Enter 3rd favourtie movie:")
+
+# l=[m1,m2,m3]
+# print(l)
+
+#palindrome
+p=[1,2,3,1]
+p2=p.copy()
+p2.reverse()
+if p==p2:
+    print("palindrome")
+else:
+    print("not palindrome")
+
+
