cbc1aca85d334ebedf63894ac672fb641f4b2e1e
Musab <joiya4636@gmail.com>
2025-04-14 00:21:49 -0700

    Day 3 List

---
+++ Basics/Day3/List.py
@@ -0,0 +1,26 @@
+#built in data type stores set of values
+marks= [12,34,53,54,23]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(len(marks))
+student=['musab',76,19]
+print(student)
+# lists are mutable while strings are mutable
+student[0]='joiya'
+print(student)
+# print(student[3]) out of range
+# for slicing there are same rules as string
+print(marks[2:4])
+print(marks[2:])
+print(marks[-4:-1])
+
+
+# List methods
+student.append(4)
+# student.sort()  not suportd for int and str
+# student.sort(reverse=True) not supported
+student.reverse()
+student.insert(2,15) #(index,element)
+print(student)
