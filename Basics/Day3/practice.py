86bc03f23c07ec3a87ecdbd7f59f477278d0d9ba
Musab <joiya4636@gmail.com>
2025-04-14 00:38:46 -0700

    Update practice.py

--- Basics/Day3/practice.py
+++ Basics/Day3/practice.py
@@ -1,12 +1,12 @@
-# m1=input("Enter 1st favourtie movie:")
-# m2=input("Enter 2nd favourtie movie:")
-# m3=input("Enter 3rd favourtie movie:")
+m1=input("Enter 1st favourtie movie:")
+m2=input("Enter 2nd favourtie movie:")
+m3=input("Enter 3rd favourtie movie:")
+l=[m1,m2,m3]
+print(l)
 
-# l=[m1,m2,m3]
-# print(l)
 
 #palindrome
-p=[1,2,3,1]
+p=[1,2,1]
 p2=p.copy()
 p2.reverse()
 if p==p2:
@@ -15,3 +15,10 @@ else:
     print("not palindrome")
 
 
+
+grades=["C","D","A","A","B","B","A"]
+print(grades.count("A"))
+
+
+grades.sort()
+print(grades)
