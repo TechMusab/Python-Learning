e6c0f9ff4664fd5e29f97efa583210ffaa70c716
Musab <joiya4636@gmail.com>
2025-04-15 01:36:39 -0700

    Day 4 Dictionary

---
+++ Basics/Day 4/dictionary.py
@@ -0,0 +1,11 @@
+#key valur pair
+# word-> meaning
+info={
+    "key":"value",
+    "name":"musab",
+    "learning":"Coding",
+    "age":"18",
+    "marks":[23,43,55],
+    "topics":("dict","set")
+}
+print(info["topics"])
