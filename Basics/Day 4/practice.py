07710bf08401771ccfaebcfb3c292bd2a1564512
Musab <joiya4636@gmail.com>
2025-04-15 02:14:05 -0700

    Day 4 completed

---
+++ Basics/Day 4/practice.py
@@ -0,0 +1,19 @@
+# dict={
+#     "table":["a piece of furniture","list of facts and figures"],
+#     "cat":"a small animal"
+# }
+# s={"c++","python","java","pyhton","javascript","java","pyhton","java","c++","c"}
+# print(len(s))
+# marks={}
+# m1=int(input("Enter marks of physics:"))
+# m2=int(input("Enter marks of chemistry:"))
+# m3=int(input("Enter marks of maths:"))
+# subjects={
+#     "physics":m1,
+#     "chemistry":m2,
+#     "maths":m3
+#           }
+# marks.update(subjects)
+# print(marks)
+s2={9,"9.0"}
+print(s2)
--- Basics/Day 4/set.py
+++ Basics/Day 4/set.py
@@ -19,3 +19,7 @@ print(s1)
 s1.add((1,2,3))
 s1.clear()
 #s1.pop() removes a random value
+a1={1,2,3}
+a2={2,1,4}
+print(a1.union(a2))
+print(a1.intersection(a2))
