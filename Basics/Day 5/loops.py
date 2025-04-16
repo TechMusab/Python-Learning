a9c3fba059930630381f8f4e7df743482217e084
Musab <joiya4636@gmail.com>
2025-04-16 06:42:50 -0700

    Day 5 Loops

---
+++ Basics/Day 5/loops.py
@@ -0,0 +1,31 @@
+#while loop
+# count=1
+# while count<=100:
+#     print(count)
+#     count+=1
+# i=100
+# while i>=0:
+#     print(i)
+#     i-=1
+# n=int(input("Enter a number: "))
+# i=1
+# while i<=10:
+#     print(i," * ",n," = ", i*n)
+#     i=i+1
+
+# i=1
+# while i<=10:
+#     print(i*i)
+#     i+=1
+# tup=[1,4,9,16,25,36,49,64,81,100]
+# n=int(input("Enter a number: "))
+# i=0
+# while i<=len(tup):
+#     if(n==tup[i]):
+#         print("Number found at index",i)
+#         break
+#     i+=1
+
+
+for i in range(2,12,2):
+    print(i)
