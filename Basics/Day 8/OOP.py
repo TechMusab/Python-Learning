1233ecb5f612c69c223332241edd890e5b5c4cb9
Musab <joiya4636@gmail.com>
2025-04-20 14:25:09 -0700

    Update OOP.py

--- Basics/Day 8/OOP.py
+++ Basics/Day 8/OOP.py
@@ -53,23 +53,167 @@
 # 3.Inheritance
 # 4.Polymorphism
 
-class Account:
-    def __init__(self, no, balance):
-        self.no = no
-        self.balance = balance
+# class Account:
+#     def __init__(self, no, balance):
+#         self.no = no
+#         self.balance = balance
 
-    def deposit(self, amount):
-        self.balance += amount
-        print("Deposited:", amount)
+#     def deposit(self, amount):
+#         self.balance += amount
+#         print("Deposited:", amount)
         
-    def withdraw(self, amount):
-        self.balance-=amount
-        print("Withdrawn:", amount)
+#     def withdraw(self, amount):
+#         self.balance-=amount
+#         print("Withdrawn:", amount)
     
-    def get_balance(self):
-        print("Balance:", self.balance)
+#     def get_balance(self):
+#         print("Balance:", self.balance)
 
-a1= Account(101, 1000)
-a1.deposit(500)
-a1.withdraw(200)
-a1.get_balance()
+# a1= Account(101, 1000)
+# a1.deposit(500)
+# a1.withdraw(200)
+# a1.get_balance()
+
+# del keyword
+# del a1
+# print(a1)
+
+# class Account:
+#     def __init__(self, no, balance,accpass):
+#         self.no = no
+#         self.balance = balance
+#         self.__accpass=accpass
+     
+#     def resetpass(self, newpass):
+#         self.__accpass=newpass
+#         print("Password changed successfully")
+
+
+# a1= Account(101, 1000, "abc123")
+# a1.resetpass("xyz456")
+# #print(a1.__accpass) # this will give error
+
+# # inheritance
+
+# class Parent:
+#     def __init__(self):
+#         print("Parent constructor")
+    
+#     def show(self):
+#         print("Parent class method")
+
+# class Child(Parent):
+#     def __init__(self):
+#         super().__init__() 
+#         print("Child constructor")
+    
+#     def show(self):
+#         super().show() 
+#         print("Child class method")
+
+# c1=Child()
+# c1.show()
+
+# single inhertance
+# class Parent:
+#     def __init__(self):
+#         print("Parent constructor")
+    
+#     def show(self):
+#         print("Parent class method")
+# class Child(Parent):
+#     def __init__(self):
+#         super().__init__() 
+#         print("Child constructor")
+    
+#     def show(self):
+#         super().show() 
+#         print("Child class method")
+# c1=Child()
+# c1.show()
+
+# Multi level inheritance
+# class Parent:
+#     def __init__(self):
+#         print("Parent constructor")
+    
+#     def show(self):
+#         print("Parent class method")
+# class Child(Parent): 
+#     def __init__(self):
+#         super().__init__() 
+#         print("Child constructor")
+    
+#     def show(self):
+#         super().show() 
+#         print("Child class method")
+# class GrandChild(Child):
+#     def __init__(self):
+#         super().__init__() 
+#         print("GrandChild constructor")
+    
+#     def show(self):
+#         super().show() 
+#         print("GrandChild class method")
+# g1=GrandChild()
+# g1.show()
+
+# Muliple inheritance
+class Parent1:
+    def __init__(self):
+        print("Parent1 constructor")
+    
+    def show(self):
+        print("Parent1 class method")
+class Parent2:
+    def __init__(self):
+        print("Parent2 constructor")
+    
+    def show(self):
+        print("Parent2 class method")
+class Child(Parent1, Parent2):
+    def __init__(self):
+        super().__init__() 
+        print("Child constructor")
+    
+    def show(self):
+        super().show() 
+        print("Child class method")
+c1=Child()
+c1.show()
+
+# class method
+class Person:
+    name="anonymous"
+
+    # def change_name(self, new_name):
+    #     self.__class__.name=new_name
+    @classmethod
+    def change_name(cls, new_name):
+        cls.name=new_name
+
+p1=Person()
+p1.change_name("Musab")
+print(Person.name)
+# 3 types of methods
+# 1. instance method
+# 2. class method
+# 3. static method
+
+# @ property decorator
+class Student:
+    def __init__(self,phy,chem,math):
+        self.phy=phy
+        self.chem=chem
+        self.math=math
+    
+    @property
+    def percentage(self):
+        return str((self.phy+self.chem+self.math)/3) + "%"
+
+s1=Student(90,80,70)
+print(s1.percentage)
+s1.phy=66
+print(s1.percentage)
+
+# Polymorphism
