ec8933821789da934214033565cd55404d0c0cb1
Musab <joiya4636@gmail.com>
2025-04-20 13:09:33 -0700

    Update OOP.py

--- Basics/Day 8/OOP.py
+++ Basics/Day 8/OOP.py
@@ -1,12 +1,18 @@
 # to prevent redundency and increase reusability
 class Student:
-    def __init__(self,name):
+    #default constructor
+    def __init__(self):
+        pass
+    #parametrized constructor
+    def __init__(self,name,marks):
         self.name=name
+        self.marks=marks
         print("Adding new student in Database...")
     
 
-s1=Student("Musab")
+s1=Student("Musab",98)
 print(s1.name)
+print(s1.marks)
 # print(s1)
 
 
