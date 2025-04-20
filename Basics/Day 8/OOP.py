f3bef623a35ac7058d79807823176456e2865953
Musab <joiya4636@gmail.com>
2025-04-20 12:38:31 -0700

    Update OOP.py

--- Basics/Day 8/OOP.py
+++ Basics/Day 8/OOP.py
@@ -1,3 +1,14 @@
 #to prevent redundency and increase reusability
-class Student:
+# class Student:
+#     name="Musab"
+
+# s1=Student()
+# print(s1.name)
+# print(s1)
+
+
+class  Car:
+    color="blue"
     
+c=Car()
+print(c.color)    
