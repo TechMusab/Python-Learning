1954e5cefd2c697620da8c44efc327b19f6921f9
Musab <joiya4636@gmail.com>
2025-04-26 09:28:02 -0700

    Update day1.py

--- Pandas/day1.py
+++ Pandas/day1.py
@@ -2,8 +2,8 @@ import pandas as pandas
 #Load Dataset
 df=pandas.read_csv("sales_data.csv",encoding="latin1")
 jsondata=pandas.read_json("sales_data.json",encoding="latin1")
-# print(df)
-# print(jsondata)
+print(df)
+print(jsondata)
 #save data
 data={
     "name":["John","Doe","Jane"],
@@ -12,10 +12,10 @@ data={
     "salary":[70000,80000,60000],
 }
 df2=pandas.DataFrame(data)
-# print(df2)
-# df2.to_csv("output.csv",index=False)
-# df2.to_excel("output.xlsx",index=False)
-# df2.to_json("output.json",index=False)
+print(df2)
+df2.to_csv("output.csv",index=False)
+df2.to_excel("output.xlsx",index=False)
+df2.to_json("output.json",index=False)
 # Selecting single column
 print(df2["age"])
 # Selecting multiple columns
