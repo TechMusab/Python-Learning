925aa5f7812d06483487ab1394c300c622db0341
Musab <joiya4636@gmail.com>
2025-04-27 11:30:53 -0700

    Update day2.py

--- Pandas/Day2/day2.py
+++ Pandas/Day2/day2.py
@@ -1,8 +1,8 @@
 import pandas as pandas
 data={
-    'Name': ['John', 'Anna', 'Peter', 'Linda'],
-    'Age': [28, 24, 35, 32],
-    'salary':[50000, 60000, 70000, 80000]
+    'Name': ['John',None, 'Anna', 'Peter', 'Linda'],
+    'Age': [28,None, 24, 35, 32],
+    'salary':[50000,None, 60000, 70000, 80000]
 }
 df=pandas.DataFrame(data)
 print(df)
@@ -13,4 +13,15 @@ df.insert(2, "Bonus", df["salary"] * 0.1)
 print(df)
 #updating data
 df.loc[1, "Bonus"] = 10000
+print(df)
+#Updating Salary
+df["salary"]=df["salary"]*1.1
+print(df)
+#Removing column
+df.drop(columns=["Bonus","Age"],inplace=True)
+print(df)
+# Checking Missing data
+print(df.isnull().sum())
+# Handling Missing Data
+df.dropna(axis=0,inplace=True)
 print(df)
