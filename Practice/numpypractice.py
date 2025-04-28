6f1da66012ae07abb17e73162ba1c515333caad4
Musab <joiya4636@gmail.com>
2025-04-28 09:54:09 -0700

    Update numpypractice.py

--- Practice/numpypractice.py
+++ Practice/numpypractice.py
@@ -1,16 +1,23 @@
 import numpy as np 
+
+#Create a 5×5 matrix with random integers.
 arr=np.random.randint(1,100,size=(5,5))
-#np.where(condition,true,false)
+#Replace all odd numbers with -1.
 matrixafter=np.where(arr%2 !=0,-1,arr)
 print(arr)
 print("\nMatrix after replacing odd numbers with -1:")
 print(matrixafter)
+#Find the row-wise maximum and minimum.
 maxrow=np.max(arr,axis=1)
+minrow=np.min(arr,axis=1)
 print("\nMax of each row:")
 print(maxrow)
-# Create 100 evenly spaced numbers from 0 to 10
+print('\nMin of each row:')
+print(minrow)
+#Create an array of 100 evenly spaced numbers between 0 and 10.
 evenly_spaced = np.linspace(0, 10, 100)
 print("\nArray of 100 evenly spaced numbers between 0 and 10:")
 print(evenly_spaced)
+#Generate a 3x3 identity matrix.
 identity=np.ones((3,3))
 print(identity)
