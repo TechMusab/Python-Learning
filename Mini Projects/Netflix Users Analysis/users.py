3c7922797aabcbc3c337b94d30ba3e1c527f0dee
Musab <joiya4636@gmail.com>
2025-05-03 09:51:46 -0700

    Update users.py

--- Mini Projects/Netflix Users Analysis/users.py
+++ Mini Projects/Netflix Users Analysis/users.py
@@ -7,9 +7,8 @@ data=pandas.read_csv('data.csv')
 df=pandas.DataFrame(data)
 df['datatime']=pandas.to_datetime(df['datetime'],format='%Y-%m-%d %H:%M:%S',errors='coerce')
 df['release_date']=pandas.to_datetime(df['release_date'],format='%Y-%m-%d',errors='coerce')
-df['duration'] = pandas.to_numeric(df['duration'], errors='coerce') # errors ='coerce' will convert non-numeric values to NaN
+df['duration'] = pandas.to_numeric(df['duration'], errors='coerce') 
 
-#Feature Engineering
 user_data = df.groupby('user_id').agg({
     'duration': ['sum', 'mean', 'count'],
     'genres': lambda x: len(set(','.join(x).split(',')))
@@ -17,6 +16,7 @@ user_data = df.groupby('user_id').agg({
 print(user_data)
 user_data.columns = ['user_id', 'total_watch_time', 'avg_watch_time', 'session_count', 'unique_genres']
 print(user_data)
+
 # Clustering
 features = user_data[['total_watch_time', 'avg_watch_time', 'session_count', 'unique_genres']]
 scaled = StandardScaler().fit_transform(features)
